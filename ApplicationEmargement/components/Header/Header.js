387ab8def756a3f84e81810ba37b8ffd711a4fd2
yohannlc <yohann.lecamx@gmail.com>
2023-03-20 17:14:50 +0100

    Arrow dans le header

--- ApplicationEmargement/components/Header/Header.js
+++ ApplicationEmargement/components/Header/Header.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 export default function Header() {
     return (
         <View style={styles.container}>
+                <Image source={require('./arrow.png')} style={styles.arrow} />
             <Image source={require('./logo.png')} style={styles.logo} />
             <Image source={require('./menu.png')} style={styles.menu} />
         </View>
@@ -17,12 +18,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
+    arrow: {
+        marginTop: 12,
+        width: 22,
+        height: 22,
+    },
     logo: {
         width: 200,
         height: 50,
     },
     menu: {
-        width: 40,
-        height: 40,
+        marginTop: 5,
+        width: 35,
+        height: 35,
     },
+    
 });
