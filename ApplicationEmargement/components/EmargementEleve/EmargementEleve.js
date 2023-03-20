9e2a89456885646fd2e44414fd2fb65c03c300a4
Clement YZIQUEL <clement.yziquel@isen-ouest.yncrea.fr>
2023-03-20 16:21:51 +0100

    "routage" OK pour intervenant et élève

--- ApplicationEmargement/components/EmargementEleve/EmargementEleve.js
+++ ApplicationEmargement/components/EmargementEleve/EmargementEleve.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import BoutonScanner from "../BoutonScanner/BoutonScanner";
+import ListeSessionsEleve from "../ListeSessionsEleve/ListeSessionsEleve";
 
-
+/*
+ * Emargement de l'élève
+ *
+ * props:
+ * - sessionId: id de la session en cours
+ * - session: session en cours
+ */
 export default function EmargementEleve(props) {
-
     return (
         <View style={styles.emargementEleve} >
+            <View>
+                <ListeSessionsEleve sessions={props.session} setSessionId={props.setSessionId} setSession={props.setSession} setEmargementEnCours={props.setEmargementEnCours}/>
+            </View>
             <Text style={styles.text}>Émargement</Text>
             <View style={styles.button} >
                 <BoutonScanner />
